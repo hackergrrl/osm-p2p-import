fec16692fc792da4a66c295c0639f94d94232538
Stephen Whitmore <sww@eight45.net>
2017-10-04 13:11:46 -0700

    update test

--- test/basic.js
+++ test/basic.js
@@ -1,25 +1,14 @@
 var test = require('tape')
-var importer = require('../')
 var fs = require('fs')
-var mkdirp = require('mkdirp')
-var rimraf = require('rimraf')
-var memdb = require('memdb')
+var importer = require('../')
+var OsmMem = require('osm-p2p-mem')
 
 test('smaller.xml', function (t) {
   var xml = fs.createReadStream('./test/smaller.xml')
-  rimraf.sync('/tmp/osm-p2p-ex')
-  mkdirp.sync('/tmp/osm-p2p-ex')
 
-  importer('/tmp/osm-p2p-ex', xml, function (err) {
-    t.error(err)
-    t.end()
-  })
-})
+  var osm = OsmMem()
 
-test('smaller.xml: memdb', function (t) {
-  var xml = fs.createReadStream('./test/smaller.xml')
-
-  importer.toLevel(memdb(), xml, function (err) {
+  importer(osm, xml, function (err) {
     t.error(err)
     t.end()
   })
